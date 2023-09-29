e3fab425e18699b44c69254afdb553c1c9571599
HanningtoneM <hanningtonepompyy@gmail.com>
2023-09-29 15:10:37 +0300

    Add  lefrt and right drag functionality

--- script.js
+++ script.js
@@ -10,5 +10,17 @@ for(list of lists){
         right.addEventListener("dragover", function(e){
             e.preventDefault();
         })
+        right.addEventListener("drop", function(e){
+            right.appendChild(selected);
+            selected = null;
+        })
+
+        left.addEventListener("dragover", function(e){
+            e.preventDefault();
+        })
+        left.addEventListener("drop", function(e){
+            left.appendChild(selected);
+            selected = null;
+        })
     })
 }

a9ea2a29808804dacdccdd2990c7ccec1e0c1510
HanningtoneM <hanningtonepompyy@gmail.com>
2023-09-29 15:05:37 +0300

    Add for function

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const lists = document.getElementsByClassName("list")
 const right = document.getElementById("right")
 const left = document.getElementById("left")
+
+
+for(list of lists){
+    list.addEventListener("dragstart", function(e){
+        let selected = e.target;
+
+        right.addEventListener("dragover", function(e){
+            e.preventDefault();
+        })
+    })
+}
